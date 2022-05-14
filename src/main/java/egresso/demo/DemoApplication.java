c18125f2b5ad0d1cffba64262a59511285d07169
Durand <junior_durand@outlook.com>
2022-05-14 17:13:30 -0300

    feature(add-spring-project-files): add project files.

---
+++ src/main/java/egresso/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package egresso.demo;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// import egresso.entity.User;
+// import egresso.entity.repository.UserRepo;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+	// @Autowired
+	// private static UserRepo r;
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+			
+		// User usuario = new User();
+
+		// // usuario.getEmail();
+
+		// // r.save(usuario);
+		
+	}
+
+}
